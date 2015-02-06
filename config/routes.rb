f728cafc0646854b5cdbcc5c0b8613dee4964fff
James F Odeh <thenamesviper@gmail.com>
2015-02-05 23:05:46 -0800

    Tagging features completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Blogger::Application.routes.draw do
   resources :articles do
     resources :comments
+    resources :tags
   end
   root to: "articles#index"
+  resources :tags do
+    resources :articles
+  end
 end
